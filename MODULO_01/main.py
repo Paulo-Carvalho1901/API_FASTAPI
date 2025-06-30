f99648de1c5b73a90ff48157aa33595433d3c3f2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-30 17:52:38 -0300

    salvando a chamada da api "COMANDO" e o arquivo de requirements.txt

--- MODULO_01/main.py
+++ MODULO_01/main.py
@@ -11,3 +11,6 @@ app = FastAPI()
 async def raiz():
     return {'msg': 'Curso de Fastapi completo!'}
 
+
+# Chamando a execução da aplicação "uvicorn main:app"
+# http://127.0.0.1:8000 ou http://localhost:8000
