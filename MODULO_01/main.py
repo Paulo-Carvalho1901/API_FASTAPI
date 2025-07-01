a10c7b5abfc62f3b88b9d810e47d8e524e6e5dd3
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-01 01:23:18 -0300

    modulo basico primeiro contatos api

--- MODULO_01/main.py
+++ MODULO_01/main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
 
 
+
 # Estanciando o Objeto 
 app = FastAPI()
 
 
 # Criando endpoint (criando as ROTAS)
-@app.get('/')
+@app.get('/mensagem')
 # Criando função raiz
-async def raiz():
+async def mensagem():
     return {'msg': 'Curso de Fastapi completo!'}
 
 
 # Chamando a execução da aplicação "uvicorn main:app"
-# http://127.0.0.1:8000 ou http://localhost:8000
+# http://127.0.0.1:8000 o mesmo -> http://localhost:8000
+
+
+# Criando entrada para execução da aplicação
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, 
+                log_level='info', reload=True)
