309906052374c715608c7abd5e36f5ad4eb478c9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-06 22:17:59 -0300

    docs: explicando pydantic

--- MODULO_02/models.py
+++ MODULO_02/models.py
@@ -1,3 +1,18 @@
+"""
+O que é o Pydantic?
+Pydantic é uma biblioteca Python usada para:
+
+Validar dados automaticamente
+Garantir o tipo correto das informações
+Converter valores para o tipo esperado
+Criar modelos de dados estruturados
+
+Ele é muito usado em APIs (como com FastAPI) para definir a estrutura 
+dos dados que entram e saem da aplicação.
+
+Pydantic funciona baseado em tipagem (type hints do Python).
+
+"""
 """Optional indica que um atributo pode ter valor ou não (pode ser None)"""
 from typing import Optional # Notação de tipos
 
