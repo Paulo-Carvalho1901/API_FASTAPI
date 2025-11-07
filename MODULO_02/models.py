075cbf271d65e47aa4629b6638a5fc8dee59e678
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-06 22:16:18 -0300

    docs: add alguns comentarios

--- MODULO_02/models.py
+++ MODULO_02/models.py
@@ -1,5 +1,8 @@
+"""Optional indica que um atributo pode ter valor ou não (pode ser None)"""
 from typing import Optional # Notação de tipos
 
+
+"""Quando criamos uma classe herdando dela, ganhamos automaticamente validação de dados"""
 from pydantic import BaseModel
 
 
