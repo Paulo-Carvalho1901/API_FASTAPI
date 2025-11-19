a23c38ca4f3285b1e3d81bf41e38b4e612a23ee7
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-18 23:53:20 -0300

    feat: (api) criado rota POST inserindo cursos

--- MODULO_02/main.py
+++ MODULO_02/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException, status
 
+from models import Curso
+
 
 app = FastAPI() # instanciando FastAPI
 
@@ -34,7 +36,15 @@ async def get_curso(curso_id: int): # utilizando o parâmetro curso_id e informa
         return curso # retorno variavel 'curso' que recebe o cursos (banco Fake em memória) atualizado 
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado!') # HTTPException tratando as except e os statudos_code
-   
+
+
+# Criando endpoint POST
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return curso
 
 
 # Criando chamada de execução
